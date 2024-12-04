9339e565dd70d3bbf7c8182fdbfe13b33917342f
Alistair Baco <alistair.baco@universite-paris-saclay.fr>
2024-12-04 11:53:22 +0100

    Modification des noms de fichiers de images

--- vues/reponseInscription.php
+++ vues/reponseInscription.php
@@ -4,54 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Upload</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            padding: 20px;
-        }
-        form {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-        }
-        input[type="file"] {
-            margin-bottom: 10px;
-        }
-        input[type="submit"] {
-            background-color: #28a745;
-            color: white;
-            border: none;
-            padding: 10px 15px;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-        input[type="submit"]:hover {
-            background-color: #218838;
-        }
-    </style>
 </head>
 <body>
 
 <pre>
 <?php
 $target_file ="";
+require_once("../config/connexion.php");
+require_once("../modele/utilisateur.php");
+
+Connexion::connect();
+$requete = "SELECT max(idUtilisateur)+1 FROM `Utilisateur` WHERE 1; ";
+$resultat = Connexion::pdo()->query($requete);
+$idMax=$resultat->fetchColumn();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Specify the directory where images will be uploaded
+    
     $target_dir = "../img/profilePicture/";
     $target_file = $target_dir . basename($_FILES["u_photo"]["name"]);
+    $rename=$target_dir . (string) $idMax; 
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
     print_r($_FILES);
-
+    
     if (move_uploaded_file($_FILES["u_photo"]["tmp_name"], $target_file)) {
         echo "The file " . htmlspecialchars(basename($_FILES["u_photo"]["name"])) . " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
+    rename($target_file,$rename);
 }
 echo "\n";
 foreach($_POST as $val ){
@@ -61,7 +44,7 @@ foreach($_POST as $val ){
 ?>
 </pre>
 
-<img src="<?=$target_file?>"/>
+<img src="<?=$rename?>"/>
 
 </body>
 </html>
