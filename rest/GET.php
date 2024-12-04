00ff908fffd8cb6d45824eccfba218e86a700d3f
Calliixte <alicee.baco@gmail.com>
2024-12-04 19:57:19 +0100

    Globalisation de GET pour ne plus avoir besoin de GETVote, toujours les pbs d'utf 8 à gerer par contre

--- rest/GET.php
+++ rest/GET.php
@@ -1,13 +1,22 @@
 <?php
 
 require_once("../config/connexion.php");
+require_once("../modele/groupe.php");
 Connexion::connect();
 $classe = $_GET["classe"];
 require_once("../modele/". $classe . ".php");
-$id = (int) $_GET["id"];
-if(isset($_GET["idVotant"])){
-    $idVotant= (int) $_GET["idVotant"];
-    $JSON = $classe::getJSON($id,$idVotant);
+$id = $_GET["id"];
+if($classe == "vote"){
+    if(!isset($_GET["idVotant"])){
+        echo "Vous devez indiquer un idVotant pour recuperer un Vote";
+    }
+    if(!isset($_GET["idGroupe"])){
+        echo "Vous devez indiquer un idGroupe pour recuperer un Vote";
+    }
+    $idVotant= $_GET["idVotant"];
+    $idGroupe= $_GET["idGroupe"];
+    $groupe = Groupe::getGroupe($idGroupe);
+    $JSON = Vote::getJSON($id,$idVotant,$groupe);
 }
 else{
 $JSON = $classe::getJSON($id);
--- rest/GETVote.php
+++ rest/GETVote.php
@@ -3,7 +3,7 @@ require_once("../config/connexion.php");
 require_once("../modele/vote.php");
 require_once("../modele/groupe.php");
 Connexion::connect();
-
+ //classe purement pour du debug, à ne pas utiliser, GET fonctionne très bien sans 
 $id = $_GET["id"];
 $idVotant= $_GET["idVotant"];
 $idGroupe= $_GET["idGroupe"];
