92e41d2ef6160b029a547eba41bd5cd9a237c24f
Alain Sandoz <alain.sandoz@universite-paris-saclay.fr>
2024-12-04 09:59:18 +0100

    getGroupe

--- modele/groupe.php
+++ modele/groupe.php
@@ -11,7 +11,17 @@
             }
         }
 
-        public function ___toString(){
+        public static function getGroupe($idGroupe){
+            $requete = "SELECT idGroupe, nomGroupe FROM Groupe WHERE idGroupe = $idGroupe;";
+            $resultat = Connexion::pdo()->query($requete);
+            $resultat->setFetchmode(PDO::FETCH_CLASS,"Groupe");
+            
+            return $resultat->fetch();
+        }
+
+        
+
+        public function __toString(){
             return "<h3> Groupe </h3>
                     <p>id : $this->idGroupe<br>
                        nom : $this->nomGroupe</p>";
