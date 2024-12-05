dbefb60f4fdfde79e01b856757bc8e550cd8a942
Calliixte <alicee.baco@gmail.com>
2024-12-05 17:43:41 +0100

    J'ai littéralement rajouté un commentaire mais bon quand on arrete de travail faut commit alors je commit

--- rest/GETUtilisateur.php
+++ rest/GETUtilisateur.php
@@ -5,6 +5,7 @@ require_once("../modele/utilisateur.php");
 Connexion::connect();
 $idU = (int)$_GET["id"];
 $UserJSON = Utilisateur::getJSON($idU);
+ //classe purement pour du debug, à ne pas utiliser, GET fonctionne très bien sans 
 
 echo $UserJSON;
 
