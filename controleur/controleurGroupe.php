fe671825afdfdc201ae45d1c0559c2a73388d46f
Alain Sandoz <alain.sandoz@universite-paris-saclay.fr>
2024-12-09 16:30:30 +0100

    liste fonctions controleur groupe

---
+++ controleur/controleurGroupe.php
@@ -0,0 +1,20 @@
+<?php
+    Class controleurGroupe{
+        public static function afficherGrandGroupe(int $idGroupe){
+            echo "afficher groupe numéro $idGroupe";
+            // include("vue/groupe.php");
+        }
+
+        public static function afficherVotes(int $idGroupe){
+            echo "afficher votes du groupe numéro $idGroupe";
+        }
+        
+        public static function afficherRegle(int $idGroupe){
+            echo "les règles du groupe numéro $idGroupe";
+        }
+
+        public static function afficherPetitGroupe(int $idGroupe){
+            echo " le groupe $idGroupe en petit";
+        }
+    }
+?>
