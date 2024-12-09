da79fd4ab861bdbc36ca6d3752ea366e3997b0ac
Alistair Baco <alistair.baco@universite-paris-saclay.fr>
2024-12-09 16:30:51 +0100

    Tests avec les controleurs

--- controleur/controleurVote.php
+++ controleur/controleurVote.php
@@ -1,3 +1,35 @@
 <!-- méthodes : 
 afficherVoteGros
 afficherListePetitsVotes -->
+
+<?php
+
+class controleurVote{
+        public static function afficherVotePetit(){
+            echo "afficherVotePetit";
+        }
+        public static function afficherVoteGros(){
+            echo "afficherVoteGros";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+?>
