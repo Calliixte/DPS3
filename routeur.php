a03ce213e23196805a8d715d052679f90eb9be54
vainnye <vianney.jcqmt@gmail.com>
2024-12-09 15:32:08 +0100

    du progrès ?

--- routeur.php
+++ routeur.php
@@ -1,40 +1,38 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8"/>
-        <title>DPS3</title>
-        <link rel='stylesheet' href='css/style.css'/>
-    </head>
-    <body>
-        <main>
-            <h2>DPS3</h2>
-            <?php 
-
-            require_once("config/connexion.php");
-            
-            require_once("modele/message.php");
-            require_once("modele/reaction.php");
-            require_once("modele/utilisateur.php");
-            require_once("modele/groupe.php");
-            require_once("modele/vote.php");
-            Connexion::connect();
+<?php
 
 
-            
-
-            // $User = Utilisateur::getUtilisateur(2);
-
-            // $User->display();
-
-            $groupe = Groupe::getGroupe(1);
-
-            // $groupe->display();
-
-            include('vues/bouton-groupe.php');
 
 
-            ?>
-        </main>
-        <footer>this hell of a footer, such a banger</footer>
-    </body>
-</html>
+require_once("config/connexion.php");
+
+require_once("modele/message.php");
+require_once("modele/reaction.php");
+require_once("modele/utilisateur.php");
+require_once("modele/groupe.php");
+require_once("modele/vote.php");
+
+Connexion::connect();
+
+$utilisateur = Utilisateur::getUtilisateur(1);
+// $utilisateur->display();
+
+// $groupe = Groupe::getGroupe(1);
+// $groupe->display();
+// include('vues/boutonGroupe.php');
+
+$titre = 'DPS3';
+$styleSpecial = '';
+
+include('vues/debut.php');
+include('vues/header.html');
+echo '<main>';
+echo '<h2>DPS3</h2>';
+
+echo '<p>ici c\'est le main</p>';
+
+echo '</main>';
+include('vues/footer.html');
+include('vues/fin.html');
+
+
+?>
---
+++ vues/debut.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>$titre</title>
+        <link rel="icon" type="image/x-icon" href="/media/logo.svg">
+
+        <link type='text/css' rel='stylesheet' href='css/style.css'>
+        <?php
+            if(isset($styleSpecial) ||  $styleSpecial!='')
+                echo "<link type='text/css' rel='stylesheet' href='css/$styleSpecial.css'>";
+        ?>
+    </head>
+    <body>
