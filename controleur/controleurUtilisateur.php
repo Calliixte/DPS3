841e13239fa7cdea3c8ba0f5deda93a64fa9a039
Alistair Baco <alistair.baco@universite-paris-saclay.fr>
2024-12-09 16:39:59 +0100

    ptit push pour le frerot

--- controleur/controleurUtilisateur.php
+++ controleur/controleurUtilisateur.php
@@ -5,9 +5,11 @@
 class controleurUtilisateur{
     public static function afficherPetitUtilisateur(){
         echo "petitUtilisateur";
+        echo "<a href=routeur.php?controleur=controleurUtilisateur&action=afficherProfilUtilisateur> profil </a> ";
     }
     public static function afficherProfilUtilisateur(){
         echo "profilUtilisateur";
+        echo "<a href=routeur.php> retour </a> ";
     }
 
 }
