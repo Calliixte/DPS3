37bb2aeb3eb9916c264f4ecb744f2324aaa882f5
Calliixte <alicee.baco@gmail.com>
2025-01-02 13:14:31 +0100

    Ajout de la methode PUT a l'api, post apres manger

--- config/rest.php
+++ config/rest.php
@@ -1,5 +1,6 @@
 <?php 
 Class Rest{
+    /* Va falloir globaliser le get a un moment la quand meme*/
     public static function getGroupe(int $idGroupe){
         $requete = "SELECT idGroupe, nomGroupe FROM Groupe WHERE idGroupe = $idGroupe;";
         $resultat = Connexion::pdo()->query($requete);
@@ -26,6 +27,40 @@ Class Rest{
 
         return json_encode((array) $vote,JSON_UNESCAPED_UNICODE);
         //Vote va garder un json fucked up pour l'instant TODO : creer une fonction to_array pour Vote
+    }
+    public static function put(string $table,array $clefs , array $valeurs){
+        //ce code est assez sensible aux injections sql mais vu que l'api put et post devraient logiquement être reservées a des gestionnaires tout va bien
+        $requete = "UPDATE $table SET ";
+        while(count($clefs)>1 && count($valeurs) >1 ){ // un seul count devrait suffire car les array seront modifiés en meme temps mais au cas ou on fait les deux
+            $k = array_pop($clefs);
+            $v = array_pop($valeurs);
+            if(count($clefs) == 1 && count($valeurs) == 1){ //si on est sur la derniere boucle on ne met pas de virgule car rien ne suivra cette modification
+                $ajout = "$k = \"$v\"";
+            }
+            /*
+            Amélioration potentielle : préciser le type inséré pour assurer un bon traitement, actuellement tout est inséré entre guillement et ça marche pour les string et int
+            mais je me dis que ça risque de deconner par exemple pour les dates, donc c'est passable mais si on met une ptite lettre collée à la valeur ou la clef ça peut
+            permettre d'adapter le traitement
+            */
+            else{
+                $ajout = "$k = \"$v\", ";   
+            }
+            
+            $requete = $requete . $ajout;
+        }
+        $clef = array_pop($clefs);
+        $valDonnee=array_pop($valeurs);
+        $fin = " WHERE $clef=$valDonnee";
+        $requete=$requete . $fin;
+        echo "$requete";
+        echo "<br/>";
+        try {
+            $rowsAffected = Connexion::pdo()->exec($requete);
+            echo "Mise à jour réussie. Nombre de lignes affectées : $rowsAffected";
+        } catch (PDOException $e) {
+            echo "Erreur lors de l'exécution de la requête : " . $e->getMessage();
+        }
+
     }
 }
 
---
+++ rest/PUT.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("../config/connexion.php");
+require_once("../config/rest.php");
+foreach(glob("../modele/*php") as $fichier){
+    require_once($fichier);
+}
+
+Connexion::connect();
+$lienDocu= "<a href=\"index.html\">documentation</a>";
+if(!isset($_GET["table"])){
+    echo "<b>Erreur : </b> Pour utiliser PUT vous devez indiquer une <b>table</b> à modifier, vous pouvez consulter la  $lienDocu pour en savoir plus. ";
+    exit();
+}
+$table = $_GET["table"];
+unset($_GET["table"]); //retire table du tableau get pour les traitements qui vont suivre
+Rest::put($table,array_keys($_GET),array_values($_GET));
+
+//la deuxieme (la premiere etant la table) valeur de l'array GET sera la condition d'update, generalement on voudrait update sur une seule ligne donc ça sera idTable=idLigneVoulue
+
+?>
