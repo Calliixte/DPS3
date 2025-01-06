4b8d51862eb692aa1781f8f689468f335cd7b115
Will-Cupa <adjm.sandoz@outlook.fr>
2025-01-06 23:46:44 +0100

    css profil

--- vues/profil.php
+++ vues/profil.php
@@ -1,28 +1,33 @@
 <body>
-<?php
-echo"<div id=\"verif\">";
-if($infoUtilisateur["estVerifie"]){
-    echo "<p>Votre profil est vérifié ✅</p>";
-}else{
-    echo "<p>Votre profil n'est pas vérifié ❌</p>";
-}
-echo"</div>";
-echo "<h2> Mes informations </h2>";
-echo"<div>";
-$prenom =$infoUtilisateur["prenom"];
-$nom =$infoUtilisateur["nom"];
-$pseudo = $infoUtilisateur["pseudo"];
-$mail =$infoUtilisateur["mail"];
-$adresse =$infoUtilisateur["adresse"];
-$dateNaissance =$infoUtilisateur["dateNaissance"];
+    <div id="default">
+        <h1>Profil et informations</h1>
+        <div id="profil">
+            <?php
+            echo"<div id=\"verif\">";
+            if($infoUtilisateur["estVerifie"]){
+                echo "<p>Votre profil est vérifié</p> <p class=\"emote\">✅</p>";
+            }else{
+                echo "<p>Votre profil n'est pas vérifié</p> <p class=\"emote\">❌</p>";
+            }
+            echo"</div>";
+            echo "<h2> Mes informations </h2>";
+            echo"<div id=\"userInfo\">";
+            $prenom =$infoUtilisateur["prenom"];
+            $nom =$infoUtilisateur["nom"];
+            $pseudo = $infoUtilisateur["pseudo"];
+            $mail =$infoUtilisateur["mail"];
+            $adresse =$infoUtilisateur["adresse"];
+            $dateNaissance =$infoUtilisateur["dateNaissance"];
 
-echo "<p>Prénom : $prenom</p>";
-echo "<p>Nom : $nom</p>";
-echo "<p>Pseudo : $pseudo</p>";
-echo "<p>E-mail : $mail</p>";
-echo "<p>Adresse : $adresse</p>";
-echo "<p>Date de naissance : $dateNaissance</p>";
-echo "</div>";
-?>
-
+            $class = "infoValue"; //Plus facile à manipuler de cette manière
+            echo "<p>Prénom :</p> <p class=$class>$prenom</p>";
+            echo "<p>Nom :</p> <p class=$class>$nom</p>";
+            echo "<p>Pseudo :</p> <p class=$class>$pseudo</p>";
+            echo "<p>E-mail :</p> <p class=$class>$mail</p>";
+            echo "<p>Adresse :</p> <p class=$class>$adresse</p>";
+            echo "<p>Date de naissance :</p> <p class=$class>$dateNaissance</p>";
+            echo "</div>";
+            ?>
+        </div>
+    </div>
 </body>
