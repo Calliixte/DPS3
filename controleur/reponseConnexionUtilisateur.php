11916b159b8170eb5ec747d1b12381254a26874f
Will-Cupa <adjm.sandoz@outlook.fr>
2025-01-07 14:10:55 +0100

    fix inscription et connexion

--- controleur/reponseConnexionUtilisateur.php
+++ controleur/reponseConnexionUtilisateur.php
@@ -6,7 +6,6 @@
 
     $idUtilisateur = Utilisateur::connexion($_POST["login_utilisateur"],$_POST["password_utilisateur"]);
     if($idUtilisateur){
-        session_start();
         $_SESSION["utilisateurCourant"] = Utilisateur::getUtilisateur($idUtilisateur);
         $urlRouteur = "routeur.php?controleur=controleurApplication&action=afficherPageAccueil";
         echo "Mot de passe correct ! \n Bienvenue dans DPS3";
@@ -14,7 +13,7 @@
     }
     else{ 
         $urlErreur = "routeur.php?action=afficherConnexion&actionConnexion=Connexion";
-        echo "Mot de passe incorrect, vous allez pouvoir ressayer";
+        echo "Mot de passe incorrect, vous allez pouvoir réessayer";
         echo " <meta http-equiv=\"refresh\" content=\"1; url=$urlErreur\"> ";
 
     }  
