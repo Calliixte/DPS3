bc200d1247dfb3cea00015b11d4583a1dbaa7234
Will-Cupa <adjm.sandoz@outlook.fr>
2025-01-07 17:37:23 +0100

    choix vote gagnant

--- vues/resultatGrandVote.php
+++ vues/resultatGrandVote.php
@@ -1,13 +1,20 @@
 <h2> résultat </h2>
 <fieldset>
   <?php
-    $userRole = $_SESSION["utilisateurCourant"]->get("role");
+    $userRole = $_SESSION["utilisateurCourant"]->get("role"); 
+    $idGagnant = $_SESSION["voteCourant"]->get("idChoixGagnant") ?? -1;
 
     foreach( $listeChoixVote as $choix){
       $idChoix = $choix["idChoixVote"];
       $intitule = $choix["intitule"];
       $nbVote = $choix["nbVote"];
-      echo "<div class='boiteChoixVote'><p>$intitule : $nbVote</p></div>";
+      $class = 'boiteChoixVote';
+
+      if( $idGagnant == $idChoix ){
+        $class = $class.'Gagnant';
+      }
+
+      echo "<div class=$class><p>$intitule : $nbVote</p></div>";
 
       if($userRole == Utilisateur::ADMIN || $userRole == Utilisateur::ASSESSEUR) {
         echo "<a href='routeur.php?controleur=reponseFormulaire&action=reponse&form=ValiderGagnant&idChoix=$idChoix&idVoteInList=$idVoteActuel'>Valider Choix Gagnant</a>";
